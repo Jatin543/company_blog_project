bdf3b6e33ac4cf5f2ee431e68f69715f9420f2fb
Jatin543 <jatinpandeywork@gmail.com>
2018-10-22 16:15:43 +0530

    first commit

---
+++ blog_project/mysite/blog/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.core.urlresolvers import reverse,reverse_lazy
+from django.utils import timezone
+
+# Create your models here.
+class Post(models.Model):
+    author=models.ForeignKey('auth.User')
+    title=models.CharField(max_length=200)
+    text=models.TextField()
+    create_date=models.DateTimeField(default=timezone.now)
+    published_date=models.DateTimeField(blank=True,null=True)
+
+    def publish(self):
+        self.published_date=timezone.now()
+        self.save()
+
+    def approve_comments(self):
+        return self.comments.filter(approved_comments=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail',kwargs={'pk':self.pk})
+
+
+class Comment(models.Model):
+    post=models.ForeignKey('Post',related_name='comments')
+    author=models.CharField(max_length=200)
+    text=models.TextField()
+    create_date=models.DateTimeField(default=timezone.now)
+    approved_comments=models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comments=True
+        self.save()
+
+    def __str__(self):
+        return self.text
+
+    def get_absolute_url(self):
+        return reverse('post_list')

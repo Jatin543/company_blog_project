aaaf790e09a6f9abddff373de899e1759cda9702
Jatin <jatin@xtenav.com>
2020-08-25 00:57:09 +0530

    FIx 1

--- blog_project/mysite/blog/models.py
+++ blog_project/mysite/blog/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from django.core.urlresolvers import reverse,reverse_lazy
+from django.urls import reverse,reverse_lazy
 from django.utils import timezone
 
 # Create your models here.
 class Post(models.Model):
-    author=models.ForeignKey('auth.User')
+    author=models.ForeignKey('auth.User',on_delete=models.CASCADE)
     title=models.CharField(max_length=200)
     text=models.TextField()
     create_date=models.DateTimeField(default=timezone.now)
@@ -25,7 +25,7 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post=models.ForeignKey('Post',related_name='comments')
+    post=models.ForeignKey('Post',related_name='comments',on_delete=models.CASCADE)
     author=models.CharField(max_length=200)
     text=models.TextField()
     create_date=models.DateTimeField(default=timezone.now)
--- blog_project/mysite/mysite/urls.py
+++ blog_project/mysite/mysite/urls.py
@@ -20,7 +20,6 @@ from django.contrib.auth import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
-    url(r'accounts/login/$',views.login,name='login'),
-    url(r'accounts/logout/$',views.logout,name='logout',kwargs={'next_page':'/'}),
-
+    url(r'accounts/login/$', views.LoginView.as_view(), name='login'),
+    url(r'accounts/logout/$',views.LogoutView.as_view(),name='logout',kwargs={'next_page':'/'}),
 ]

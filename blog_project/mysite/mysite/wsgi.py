4a099e8eaa44105f7ad20158bda5eaa982b18e79
Jatin <jatin@xtenav.com>
2020-08-25 11:23:33 +0530

    fix

--- blog_project/mysite/mysite/wsgi.py
+++ blog_project/mysite/mysite/wsgi.py
@@ -7,7 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+# add the hellodjango project path into the sys.path
+sys.path.append('/home/ubuntu/skillds/company_blog_project/og_project/mysite')
+
+# add the virtualenv site-packages path to the sys.path
+sys.path.append('/home/ubuntu/skillds/myenv/Lib/site-packages')
+
+
 
 from django.core.wsgi import get_wsgi_application
 
